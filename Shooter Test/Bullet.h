25352b4a08526fda2cab0ea6500c3610c121c726
George Wellard <georgewellard03@gmail.com>
2021-06-21 21:56:41 +0100

    Shooting works

--- Shooter Test/Bullet.h
+++ Shooter Test/Bullet.h
@@ -3,43 +3,16 @@
 
 class Bullet {
 
+public:
+
+    sf::CircleShape bullet;
     sf::Vector2f currentVelocity; 
     float maxSpeed;
 
-public:
-    Bullet(sf::Vector2f size) {
-        bullet.setSize(size);
-        bullet.setFillColor(sf::Color::Blue);
-    }
+    Bullet(float radius = 5.f) : currentVelocity(0.f, 0.f), maxSpeed(15.f) {
 
-    void fire(int speed) {
-        bullet.move(speed, 0);
-    }
+        this->bullet.setRadius(radius);
+        this->bullet.setFillColor(sf::Color::Red);
 
-    int getRight() {
-        return bullet.getPosition().x + bullet.getSize().x;
     }
-
-    int getLeft() {
-        return bullet.getPosition().x;
-    }
-
-    int getTop() {
-        return bullet.getPosition().y;
-    }
-
-    int getBottom() {
-        return bullet.getPosition().y + bullet.getSize().y;
-    }
-
-    void draw(sf::RenderWindow& window) {
-        window.draw(bullet);
-    }
-
-    void setPos(sf::Vector2f newPos) {
-        bullet.setPosition(newPos);
-    }
-
-private:
-    sf::RectangleShape bullet;
 };
--- Shooter Test/Shooter Test.cpp
+++ Shooter Test/Shooter Test.cpp
@@ -2,9 +2,27 @@
 
 #include <iostream>
 #include "SFML/Graphics.hpp"
-#include "Bullet.h"
 #include "Player.h"
 #include <math.h>
+#include <vector>
+
+
+class Bullet {
+
+public:
+
+    sf::CircleShape bullet;
+    sf::Vector2f currentVelocity;
+    float maxSpeed;
+
+    Bullet(float radius = 5.f) 
+        : currentVelocity(0.f, 0.f), maxSpeed(15.f) {
+
+        this->bullet.setRadius(radius);
+        this->bullet.setFillColor(sf::Color::Red);
+
+    }
+};
 
 int main()
 {
@@ -25,11 +43,20 @@ int main()
    
 
     Player player(sf::Vector2f(50, 50));
+    Bullet b1;
+
+    std::vector<Bullet> bullets;
+    bullets.push_back(Bullet(b1));
 
     //Main Loop:
     while (window.isOpen()) {
 
         sf::Event Event;
+
+        playerCenter = sf::Vector2f(player.getX(), player.getY());
+        mousePosWindow = sf::Vector2f(sf::Mouse::getPosition(window));
+        aimDir = mousePosWindow - playerCenter;
+        aimDirNorm = aimDir / sqrt(pow(aimDir.x, 2) + pow(aimDir.y, 2));
 
         //Event Loop:
         while (window.pollEvent(Event)) {
@@ -44,14 +71,27 @@ int main()
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
                 player.move(sf::Vector2f(0, -moveSpeed));
             }
-            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
                 player.move(sf::Vector2f(0, moveSpeed));
             }
-            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
                 player.move(sf::Vector2f(-moveSpeed, 0));
             }
-            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
                 player.move(sf::Vector2f(moveSpeed, 0));
+            }
+
+            //Shooting
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                b1.bullet.setPosition(playerCenter);
+                b1.currentVelocity = aimDirNorm * b1.maxSpeed;
+
+                bullets.push_back(Bullet(b1));
+            }
+
+            for(size_t i = 0; i < bullets.size(); i++) {
+
+                bullets[i].bullet.move(bullets[i].currentVelocity);
             }
 
         }
@@ -59,6 +99,12 @@ int main()
         window.clear();
 
         player.draw(window);
+
+        for (size_t i = 0; i < bullets.size(); i++)
+        {
+            window.draw(bullets[i].bullet);
+        }
+
         window.display();
     }
 }
